4ffd1d424b738464af19c296fba359fbfa616a78
Sharifjon <sharifboymuminov@gmail.com>
2024-07-12 11:19:59 +0500

    good :)

---
+++ lib/screens/tab_box/schedule/widget/calendar.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:travel_app/utils/app_colors.dart';
+import 'package:travel_app/utils/app_size.dart';
+import 'package:travel_app/utils/app_text_style.dart';
+
+class MyDatePicker extends StatefulWidget {
+  const MyDatePicker({super.key, required this.onChangedDateTime});
+
+  final ValueChanged<DateTime> onChangedDateTime;
+
+  @override
+  MyDatePickerState createState() => MyDatePickerState();
+}
+
+class MyDatePickerState extends State<MyDatePicker> {
+  DateTime selectedDate = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.cB4BCC9.withOpacity(0.16),
+            blurRadius: 20,
+            spreadRadius: 0,
+            offset: const Offset(0, 8),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(16.we),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              children: [
+                Text(
+                  "${selectedDate.day} ${_monthName(selectedDate.month)}",
+                  style: AppTextStyle.interSemiBold.copyWith(
+                    fontSize: 20.sp,
+                    color: AppColors.c1B1E28,
+                  ),
+                ),
+                const Spacer(),
+                IconButton(
+                  icon: Icon(
+                    Icons.chevron_left,
+                    size: 22.sp,
+                    color: AppColors.c1B1E28,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      selectedDate =
+                          selectedDate.subtract(const Duration(days: 7));
+                    });
+                  },
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.chevron_right,
+                    size: 22.sp,
+                    color: AppColors.c1B1E28,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      selectedDate = selectedDate.add(const Duration(days: 7));
+                    });
+                  },
+                ),
+              ],
+            ),
+            12.getH(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: List.generate(7, (index) {
+                DateTime date = DateTime.now().subtract(
+                    Duration(days: DateTime.now().weekday - index - 1));
+                bool isSelected = date.day == selectedDate.day;
+
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      selectedDate = date;
+                      widget.onChangedDateTime.call(selectedDate);
+                    });
+                  },
+                  child: Column(
+                    children: [
+                      Text(
+                        _dayName(date.weekday),
+                        style: TextStyle(
+                          color: isSelected ? Colors.blue : Colors.grey,
+                        ),
+                      ),
+                      4.getH(),
+                      DecoratedBox(
+                        decoration: BoxDecoration(
+                          color: isSelected ? Colors.blue : Colors.transparent,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            date.day.toString(),
+                            style: TextStyle(
+                              color: isSelected ? Colors.white : Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }),
+            ),
+            16.getH(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  String _monthName(int month) {
+    switch (month) {
+      case 1:
+        return 'January';
+      case 2:
+        return 'February';
+      case 3:
+        return 'March';
+      case 4:
+        return 'April';
+      case 5:
+        return 'May';
+      case 6:
+        return 'June';
+      case 7:
+        return 'July';
+      case 8:
+        return 'August';
+      case 9:
+        return 'September';
+      case 10:
+        return 'October';
+      case 11:
+        return 'November';
+      case 12:
+        return 'December';
+      default:
+        return '';
+    }
+  }
+
+  String _dayName(int day) {
+    switch (day) {
+      case 1:
+        return 'M';
+      case 2:
+        return 'T';
+      case 3:
+        return 'W';
+      case 4:
+        return 'T';
+      case 5:
+        return 'F';
+      case 6:
+        return 'S';
+      case 7:
+        return 'S';
+      default:
+        return '';
+    }
+  }
+}
